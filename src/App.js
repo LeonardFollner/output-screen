ffb809b1750c552b95aaf79ec8ea5784cafb68d9
Leonard Follner <leonard.follner@tu-dresden.de>
2018-06-12 13:56:01 +0200

    BUGFIX: fixed empty line

--- src/App.js
+++ src/App.js
@@ -43,61 +43,61 @@ class App extends Component {
                         {events.timeslots.map((time, timeIndex) => {
                                 const eventHour = parseInt(time.split(':')[0]);
 
-                            //if (currentHour + 2 >= eventHour) {
+                            // if (currentHour + 2 >= eventHour) {
 
-                                    const nextTimeSlot = events.timeslots[timeIndex + 1];
-                                    let nextHour;
+                            const nextTimeSlot = events.timeslots[timeIndex + 1];
+                            let nextHour;
 
-                                    let nextMinute;
-                                    if (nextTimeSlot) {
-                                        nextHour = parseInt(nextTimeSlot.split(':')[0]);
-                                        nextMinute = parseInt(nextTimeSlot.split(':')[1]);
+                            let nextMinute;
+                            if (nextTimeSlot) {
+                                nextHour = parseInt(nextTimeSlot.split(':')[0]);
+                                nextMinute = parseInt(nextTimeSlot.split(':')[1]);
 
-                                    }
+                            }
 
-                                    if (nextMinute && nextHour && // current hour and next event in current hour has started
-                                        eventHour === currentHour &&
-                                        nextHour === currentHour &&
-                                        nextMinute <= currentMinute
-                                    ) {
-                                        return <td/>
-                                    }
+                            if (nextMinute && nextHour && // current hour and next event in current hour has started
+                                eventHour === currentHour &&
+                                nextHour === currentHour &&
+                                nextMinute <= currentMinute
+                            ) {
+                                return ""
+                            }
 
-                                    if (eventHour >= currentHour) {
-                                        /* const numberOfEventsAtThisTime = Object.values(events.rooms).reduce((eventCounter, room) => {
-                                            Object.keys(room.events).forEach((event) => {
-                                                if (event === time) {
-                                                    eventCounter++;
-                                                }
-                                            });
-                                            return eventCounter;
-                                        }, 0); */
+                            if (eventHour >= currentHour) {
+                                /* const numberOfEventsAtThisTime = Object.values(events.rooms).reduce((eventCounter, room) => {
+                                    Object.keys(room.events).forEach((event) => {
+                                        if (event === time) {
+                                            eventCounter++;
+                                        }
+                                    });
+                                    return eventCounter;
+                                }, 0); */
 
-                                        return (
-                                            <tr key={time}>
-                                                <td>{time}</td>
-                                                {Object.keys(events.rooms).map(roomNr => {
-                                                    const roomType = events.rooms[roomNr].type;
-                                                    const event = events.rooms[roomNr].events[time];
-                                                    if (event) {
-                                                        return (
-                                                            <td key={roomNr} colSpan={1}
-                                                                className={roomType.toLowerCase()}>
-                                                                <h2>{event.title}</h2>
-                                                                {event.details.map(detail => {
-                                                                    return (
-                                                                        <p key={detail}>{detail}</p>
-                                                                    )
-                                                                })}
-                                                            </td>
-                                                        )
-                                                    } else {
-                                                        return <td/>
-                                                    }
-                                                })}
-                                            </tr>
-                                        )
-                                    }
+                                return (
+                                    <tr key={time}>
+                                        <td>{time}</td>
+                                        {Object.keys(events.rooms).map(roomNr => {
+                                            const roomType = events.rooms[roomNr].type;
+                                            const event = events.rooms[roomNr].events[time];
+                                            if (event) {
+                                                return (
+                                                    <td key={roomNr} colSpan={1}
+                                                        className={roomType.toLowerCase()}>
+                                                        <h2>{event.title}</h2>
+                                                        {event.details.map(detail => {
+                                                            return (
+                                                                <p key={detail}>{detail}</p>
+                                                            )
+                                                        })}
+                                                    </td>
+                                                )
+                                            } else {
+                                                return <td key={roomNr}/>
+                                            }
+                                        })}
+                                    </tr>
+                                )
+                            }
                             //}
                             }
                         )}
